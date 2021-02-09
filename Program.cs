fcdba1f4268eac229553d24611cdc8042556e82d
Kevin Le <kevinle108@gmail.com>
2021-02-09 03:09:06 -0500

    done list of names

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Anna", "Barry", "Charlie", "David", "Eddie", "Frank" };
+            names.Add("Gail"); // Add "Gail" to the end of the list
+            names.Add("Harry"); // Add "Harry" to the end of the list
+            names[0] = "Andy"; // change the first item to "Andy"
+            names.RemoveAt(2); // removes the third item
+            names.Insert(4, "Paul"); // add "Paul" as the fifth item
+            names.RemoveAt(names.Count - 1); // removes the last item
+            Console.WriteLine($"the index of Paul is {names.IndexOf("Paul")}"); // finds the index of Paul
+            Console.WriteLine($"'names' is a list of strings with {names.Count} items");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
